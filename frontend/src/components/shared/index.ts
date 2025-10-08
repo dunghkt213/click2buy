78af3d6a64b972482a15ab746a2297ae9b8c59f9
NguyenDung <dung1990hk@gmail.com>
2025-10-08 20:04:19 +0700

    're_merge'

--- frontend/src/components/shared/index.ts
+++
@@ -1,3 +0,0 @@
-// Re-export shared components
-export { Categories } from '../Categories';
-export { AccountDropdown } from '../AccountDropdown';
--- frontend/src/components/sidebars/index.ts
+++
@@ -1,7 +0,0 @@
-// Re-export all sidebars from parent components directory
-export { CartSidebar } from '../CartSidebar';
-export { FilterSidebar } from '../FilterSidebar';
-export { NotificationSidebar } from '../NotificationSidebar';
-export { WishlistSidebar } from '../WishlistSidebar';
-export { PromotionSidebar } from '../PromotionSidebar';
-export { SupportSidebar } from '../SupportSidebar';
