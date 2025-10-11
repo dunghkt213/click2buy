48b1bd1bd97a78a9d6d692cfe39d3c37494c80c7
chie41 <phamvietha412005@gmail.com>
2025-10-11 17:33:52 +0700

    Update review.module.ts

--- backend/src/review/review.module.ts
+++ backend/src/review/review.module.ts
@@ -3,9 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
 import { Review, ReviewSchema } from './schemas/review.schema';
+import { UserModule } from '../user/user.module';
+import { ProductModule } from '../product/product.model';
+
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
+    UserModule,     // 👈 để Nest có thể inject UserService
+    ProductModule,  // 👈 để Nest có thể inject ProductService
+  ],
   controllers: [ReviewController],
   providers: [ReviewService],
   exports: [ReviewService],
