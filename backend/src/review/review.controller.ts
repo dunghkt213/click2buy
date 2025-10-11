7c4a8fa615af845dd492b1fb67f6e885d26474e5
chie41 <phamvietha412005@gmail.com>
2025-10-11 17:34:10 +0700

    Update review.controller.ts

--- backend/src/review/review.controller.ts
+++ backend/src/review/review.controller.ts
@@ -3,15 +3,19 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
   Query,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { QueryReviewDto } from './dto/query-review.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('reviews')
 export class ReviewController {
@@ -21,9 +25,12 @@ export class ReviewController {
    * [POST] /reviews
    * Tạo mới review
    */
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(201)
   @Post()
-  create(@Body() dto: CreateReviewDto) {
-    return this.reviewService.create(dto);
+  async create(@Body() dto: CreateReviewDto, @Req() req) {
+    const user = req.user;
+    return this.reviewService.create(dto, user._id);
   }
 
   /**
@@ -41,25 +48,31 @@ export class ReviewController {
    */
   @Get(':id')
   findById(@Param('id') id: string) {
-    return this.reviewService.findById(id);
+    return this.reviewService.findOne(id);
   }
 
   /**
    * [PATCH] /reviews/:id
    * Cập nhật review
    */
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(200)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateReviewDto) {
-    return this.reviewService.update(id, dto);
+  async update(@Param('id') id: string, @Body() dto: UpdateReviewDto, @Req() req) {
+    const user = req.user;
+    return this.reviewService.update(id, user._id, user.role, dto);
   }
 
   /**
    * [DELETE] /reviews/:id
    * Xóa review
    */
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(200)
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.reviewService.delete(id);
+  async delete(@Param('id') id: string, @Req() req) {
+    const user = req.user;
+    return this.reviewService.delete(id, user._id, user.role);
   }
 }
  
