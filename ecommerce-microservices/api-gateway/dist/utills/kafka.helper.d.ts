7a78ae74ccca3763fd8219b4bf1f8b7d183d3789
chie41 <phamvietha412005@gmail.com>
2025-11-13 11:50:23 +0700

    Del

--- ecommerce-microservices/api-gateway/dist/utills/kafka.helper.d.ts
+++
@@ -1,2 +0,0 @@
-import { ClientKafka } from '@nestjs/microservices';
-export declare function handleKafkaRequest<T = any>(kafka: ClientKafka, topic: string, payload: any): Promise<T>;
--- ecommerce-microservices/review-service/dist/src/auth/jwt.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as jwt from 'jsonwebtoken';
-export declare class JwtService {
-    private readonly secret;
-    validateToken(token: string): string | jwt.JwtPayload;
-    decodeToken(token: string): string | jwt.JwtPayload | null;
-}
