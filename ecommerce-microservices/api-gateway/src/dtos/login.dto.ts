83cd4ea973ff51fbfa3a2d141cf7c5e69e49bd31
NguyenDung <dung1990hk@gmail.com>
2025-11-15 17:57:21 +0700

    'tao_order_inventory_timeout-service_btw_done_timeout_va_order'

--- ecommerce-microservices/api-gateway/src/dtos/login.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
-
-export class LoginDto {
-    @IsString() username: string;
-    @IsString() password: string;
-}
--- ecommerce-microservices/user-service/src/auth/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const CurrentUser = createParamDecorator(
-  (_: unknown, context: ExecutionContext) => {
-    const kafkaCtx = context.switchToRpc().getContext();
-    const message = kafkaCtx.getMessage().value;
-    return message.user; // payload đã được gắn trong guard
-  },
-);
